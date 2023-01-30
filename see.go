80363ebaf12d1006171ef83a2507f475a7a844da
Krissada Sarawit <topfeekrissada@outlook.com>
2023-01-30 20:31:03 +0700

    add : help message for vs code or wt cannot run due to invalid program path.

--- see.go
+++ see.go
@@ -11,7 +11,7 @@ import (
 	"see/wt"
 )
 
-// File name : pathlist.json
+// File name : pathlist.json (in user home directory), used to store the path list and program path.
 var jsonFileName = filepath.Join(getHomeDir(), "see.json")
 
 // Get user home directory
@@ -417,6 +417,9 @@ Options:
   -re, --rename   Rename a path in pathlist
   -wt             Open the path in windows terminal
   -code           Open the path in vscode
+
+  If the program cannot run vs code or windows terminal, you can set the path of the program in the config file located at: C:\Users\%USERNAME%\.path\config.json.
+
 `
 	return message
 }
